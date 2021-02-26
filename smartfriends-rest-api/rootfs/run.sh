bb4594cf7263ec5a8622d9cf73239f3a32c85c00
Scott Downing <scott.downing@marc-cain.de>
2021-02-26 17:10:02 +0100

    Didn't properly understand the port config value

--- smartfriends-rest-api/rootfs/run.sh
+++ smartfriends-rest-api/rootfs/run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#Get port from config
-PORT=$(jq '.ports[.ports|keys[]]' /data/options.json)
-
 # Extract options data
 jq -s '.[1].SmartFriends = .[0].options.SmartFriends
     | .[1]' /data/options.json /opt/appsettings.json | sponge /opt/appsettings.json
@@ -11,4 +8,4 @@ jq -s '.[1].SmartFriends = .[0].options.SmartFriends
 [ ! -f "/lib/ld-linux-armhf.so.3" ] && [ -f "/lib/arm-linux-gnueabi/ld-2.28.so" ] && ln -s /lib/arm-linux-gnueabi/ld-2.28.so /lib/ld-linux-armhf.so.3
 
 # Run dotnet service in foreground
-dotnet /opt/SmartFriends.Host.dll --urls=http://localhost:$PORT
+dotnet /opt/SmartFriends.Host.dll --urls=http://localhost:80
