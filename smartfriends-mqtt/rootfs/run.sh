066f77aa473986a36583c050ae1a4701380f3bc9
Scott Downing <scott.downing@marc-cain.de>
2021-02-26 14:38:47 +0100

    Hassio changed the config filename to options.json

--- smartfriends-mqtt/rootfs/run.sh
+++ smartfriends-mqtt/rootfs/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Extract config data
+# Extract options data
 jq -s '.[1].SmartFriends = .[0].options.SmartFriends
     | .[1].Mqtt = .[0].options.Mqtt
-    | .[1]' /data/config.json /opt/appsettings.json | sponge /opt/appsettings.json
+    | .[1]' /data/options.json /opt/appsettings.json | sponge /opt/appsettings.json
 
 # armhf needs this linked to run dotnet
 [ ! -f "/lib/ld-linux-armhf.so.3" ] && [ -f "/lib/arm-linux-gnueabi/ld-2.28.so" ] && ln -s /lib/arm-linux-gnueabi/ld-2.28.so /lib/ld-linux-armhf.so.3
