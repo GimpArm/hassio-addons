3c5a8a2b07efbc274f6190a30877d028415d7c47
Scott Downing <scott.downing@figz.de>
2021-02-28 16:41:32 +0100

    Fixed port listening

--- smartfriends-rest-api/rootfs/run.sh
+++ smartfriends-rest-api/rootfs/run.sh
@@ -8,4 +8,4 @@ jq -s '.[1].SmartFriends = .[0].SmartFriends
 [ ! -f "/lib/ld-linux-armhf.so.3" ] && [ -f "/lib/arm-linux-gnueabi/ld-2.28.so" ] && ln -s /lib/arm-linux-gnueabi/ld-2.28.so /lib/ld-linux-armhf.so.3
 
 # Run dotnet service in foreground
-dotnet /opt/SmartFriends.Host.dll --urls=http://localhost:80
+dotnet /opt/SmartFriends.Host.dll --urls=http://[::]:80;http://0.0.0.0:80
